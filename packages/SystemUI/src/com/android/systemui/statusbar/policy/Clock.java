9c007f10cad415a790899cd75d5821293f516d0d
Lars Greiss <kufikugel@googlemail.com>
2013-11-17 07:02:45 +0100

    fb: do not call every time settings on clock intents
    only needed for configuration change.
    
    Change-Id: Ie6db0e8a3abca83f5ea6c25f39124672e06ac4ff

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -187,10 +187,11 @@ public class Clock extends TextView implements DemoMode {
                 final Locale newLocale = getResources().getConfiguration().locale;
                 if (! newLocale.equals(mLocale)) {
                     mLocale = newLocale;
-                    mClockFormatString = ""; // force refresh
                 }
+                updateSettings();
+                return;
             }
-            updateSettings();
+            updateClock();
         }
     };
 
