d66b82cf21a94c54d6dbcfa7e095e1ed1d74c7b3
camcory <camcory1@gmail.com>
2013-12-21 15:48:37 -0500

    Add Music Tile (1/2)
    Tap to play/pause. Longpress for next track
    
    Author: kufikugel
    
    Change-Id: I7bde0969adc2de15fbf8c3243d0d216006cf363a
    
    Conflicts:
    
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -36,6 +36,7 @@ public class QSConstants {
         public static final String TILE_ONTHEGO = "toggleOnTheGo";
         public static final String TILE_REBOOT = "toggleReboot";
         public static final String TILE_QUICKRECORD = "toggleQuickRecord";
+        public static final String TILE_MUSIC = "toggleMusic";
 
         public static final String TILE_DELIMITER = "|";
         public static ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
