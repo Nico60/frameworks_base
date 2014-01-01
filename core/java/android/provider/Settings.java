6b039a24a2da176a45d8cf465cd2a5d72463ef8a
garwedgess <garethwilliams21@gmail.com>
2014-01-01 17:06:46 +0100

    Move breathing SMS to settings->interface->status bar (3/3)
    Change-Id: I7b53dd65c1d6a99f13e505cf7330506974a7ffd2
    
    Conflicts:
    
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4618,6 +4618,12 @@ public final class Settings {
          */
         public static final String FORMAL_TEXT_INPUT = "formal_text_input";
 
+	/**
+         * Give SMS Notifications a breathing effect
+         * @hide
+         */
+        public static final String KEY_SMS_BREATH = "pref_key_sms_breath";
+
         /**
          * Holds the text for the Carrier label. An empty string
          * will bring the default text back.
