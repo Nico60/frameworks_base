7a6f34a78f6664137335e87e98e0dd2e5a2b9558
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2014-03-06 15:43:46 +0100

    Add option to enable/disable navring (1/2)
    This will add an option to toggle navigation ring
    
    If the navigation bar is disabled then a standalone
    navigation ring is added.
    
    Change-Id: I527c25b11bcb2c5f4b70799055f97bab65ed2cec
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4749,6 +4749,12 @@ public final class Settings {
         public static final String BATTERY_AROUND_LOCKSCREEN_RING = "battery_around_lockscreen_ring";
 
         /**
+         * Enable / disable navring
+         * @hide
+         */
+        public static final String ENABLE_NAVRING = "enable_navring";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
