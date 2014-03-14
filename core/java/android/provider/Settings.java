45ad21e6236df674dd5d3c5d8df3491be92e603d
Steve Spear <steve.spear@openpeak.com>
2014-03-13 21:57:36 -0400

    fw: Ribbons Icon Gravity
    Change-Id: I2ab6bd7231aae38a847c809e74f1374c9ddcb8ce
    Signed-off-by: Steve Spear <steve.spear@openpeak.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3520,6 +3520,7 @@ public final class Settings {
             "left_ribbon_animation_type",
             "left_ribbon_animation_duration",
             "left_ribbon_margin",
+            "left_ribbon_icon_gravity",
         };
 
         /**
@@ -3541,6 +3542,7 @@ public final class Settings {
             "right_ribbon_animation_type",
             "right_ribbon_animation_duration",
             "right_ribbon_margin",
+            "right_ribbon_icon_gravity",
         };
 
         /**
