e7436415161450f97bd94453c411ef008726348d
agm <alvaro_gutierrez_88@hotmail.com>
2014-03-22 00:18:27 +0100

    keyguard: add showCustomIntentOnKeyguard method
    Extracted from here --> https://github.com/scotthartbti/android_frameworks_base/commit/89d203acd1ed22003e6fdb8211eee9d6ee92a7ad#diff-1
    
    this is need for some slim features
    
    PS2: fix derp
    
    Change-Id: I476b94942b67843b0caab907bcee8f591740615e
    
    Conflicts:
    
    	packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardServiceWrapper.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -5319,6 +5319,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     /** {@inheritDoc} */
+    public void showCustomIntentOnKeyguard(Intent intent) {
+        if (mKeyguardDelegate != null) {
+            mKeyguardDelegate.showCustomIntent(intent);
+        }
+    }
+
+    /** {@inheritDoc} */
     public boolean inKeyguardRestrictedKeyInputMode() {
         if (mKeyguardDelegate == null) return false;
         return mKeyguardDelegate.isInputRestricted();
