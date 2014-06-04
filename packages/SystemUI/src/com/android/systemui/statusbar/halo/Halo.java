0530c2960c3893d7309430757d1661a9c38d6966
cristianomatos <cristianobmatos@gmail.com>
2014-06-04 07:35:32 -0300

    HALO: change holo blue to holo grey matching new drawables
    Change-Id: Ib62b975cee90e009ce3ca149333e9b4ea27445c4
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java

--- packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
@@ -156,7 +156,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
     private NotificationData mNotificationData;
     private String mNotificationText = "";
 
-    private Paint mPaintHoloBlue = new Paint();
+    private Paint mPaintHoloGrey = new Paint();
     private Paint mPaintWhite = new Paint();
     private Paint mPaintHoloRed = new Paint();
 
@@ -221,7 +221,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
             if (!selfChange) {
                 //mEffect.wake();
                 mBar.restartHalo();
-                //mEffect.ping(mPaintHoloBlue, HaloEffect.WAKE_TIME);
+                //mEffect.ping(mPaintHoloGrey, HaloEffect.WAKE_TIME);
                 mEffect.nap(HaloEffect.SNAP_TIME + 1000);
                 if (mHideTicker) mEffect.sleep(HaloEffect.SNAP_TIME
                                                 + HaloEffect.NAP_TIME
@@ -300,8 +300,8 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
         mTriggerPos = getWMParams();
 
         // Init colors
-        mPaintHoloBlue.setAntiAlias(true);
-        mPaintHoloBlue.setColor(0xff33b5e5);
+        mPaintHoloGrey.setAntiAlias(true);
+        mPaintHoloGrey.setColor(0xffbbbbbb);
         mPaintWhite.setAntiAlias(true);
         mPaintWhite.setColor(0xfff0f0f0);
         mPaintHoloRed.setAntiAlias(true);
@@ -1391,7 +1391,9 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
                             tick(entry, HaloEffect.WAKE_TIME * 2, 1000, true, showMsgBox);
 
                             // Pop while not tasking, only if notification is certified fresh
-                            if (mGesture != GESTURE_TASK && mState != STATE_SILENT) mEffect.ping(mPaintHoloBlue, HaloEffect.WAKE_TIME * 2);
+                            if (mGesture != GESTURE_TASK && mState != STATE_SILENT) {
+                                mEffect.ping(mPaintHoloGrey, HaloEffect.WAKE_TIME * 2);
+                            }
                         }
                     }
                     break;
@@ -1542,7 +1544,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
                                 if (mHideTicker) mEffect.sleep(HaloEffect.SLEEP_DELAY + HaloEffect.WAKE_TIME * 2, HaloEffect.SLEEP_TIME, false);
                                 boolean showMsgBox = Settings.System.getInt(resolver, Settings.System.HALO_MSGBOX, 1) == 1;
                                 tick(entry, HaloEffect.WAKE_TIME * 2, 1000, false, showMsgBox);
-                                mEffect.ping(mPaintHoloBlue, HaloEffect.WAKE_TIME * 2);
+                                mEffect.ping(mPaintHoloGrey, HaloEffect.WAKE_TIME * 2);
                                 mPingNewcomer = false;
                             }
                     }
