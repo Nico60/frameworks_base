c02df41a13d2c30c79d23b3e8cf8cf3a44b2e86f
Adnan <adnan@cyngn.com>
2014-07-14 17:38:14 -0700

    Add global setting for all stats control
    Change-Id: Iaf244d9fafbf2c47c318a90665f39d5efaea3b1b
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3226,6 +3226,13 @@ public final class Settings {
          */
         public static final String EGG_MODE = "egg_mode";
 
+        /**
+         * Global stats collection
+         *
+         * @hide
+         */
+        public static final String STATS_COLLECTION = "stats_collection";
+
        /**
          * Let Quick Settings Panel Tiles vibrate on click
          *
