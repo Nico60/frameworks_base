78955b22ed918bd153fdd2e7bfa717492093a8e6
emancebo <emancebo@cyngn.com>
2014-07-24 15:26:14 -0700

    Add system setting for t9 search input locale
    Change-Id: I780b3cca1ad2da396b76464a6dab5cc53f621ed2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5017,6 +5017,12 @@ public final class Settings {
         public static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
 
         /**
+         * Locale for secondary overlay on dialer for t9 search input
+         * @hide
+         */
+        public static final String T9_SEARCH_INPUT_LOCALE = "t9_search_input_locale";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
