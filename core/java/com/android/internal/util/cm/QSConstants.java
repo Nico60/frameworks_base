da43d97a6b9b15b47f8d7d57c336014892d7736c
Nico60 <ndhouailly@gmail.com>
2014-10-02 19:50:37 +0200

    Add NavBar as Default tile
    Change-Id: Ie1d8a030d7117f4709271f936778150256511b6a

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -59,5 +59,6 @@ public class QSConstants {
             TILES_DEFAULT.add(TILE_AIRPLANE);
             TILES_DEFAULT.add(TILE_BLUETOOTH);
             TILES_DEFAULT.add(TILE_AUTOROTATE);
+            TILES_DEFAULT.add(TILE_NAVBAR);
         }
 }
