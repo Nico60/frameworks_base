omnirom/android_frameworks_base_old

[2/2] Base: Clock on lockscreen
Allow to enable status bar clock on lockscreen (default off)

Change-Id: I80014df8544b149dbf6fb5b846d922f4ed09427e

core/java/android/provider/Settings.java
@@ -3669,6 +3669,14 @@ public final class Settings {
         public static final String STATUS_BAR_CLOCK = "status_bar_clock";
 
         /**
+         * Show or hide clock on lockscreen
+         * 0 - hide (default)
+         * 1 - show
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_LOCKSCREEN = "status_bar_clock_lockscreen";
+
+        /**
          * AM/PM Style for clock options
          * 0 - Normal AM/PM
          * 1 - Small AM/PM

Nico60/frameworks_base

Conflicts:
	core/java/android/provider/Settings.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

core/java/android/provider/Settings.java
@@ -5591,6 +5591,14 @@ public final class Settings {
         public static final String STATUS_BAR_CARRIER = "status_bar_carrier";
 
         /**
+         * Show or hide clock on lockscreen
+         * 0 - hide (default)
+         * 1 - show
+         * @hide
+         */
+        public static final String STATUS_BAR_FORCE_CLOCK_LOCKSCREEN = "status_bar_force_clock_lockscreen";
+
+        /**
          * Locale for secondary overlay on dialer for t9 search input
          * @hide
          */